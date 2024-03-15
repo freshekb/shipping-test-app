63fdb557d035c6e55c73ffdde596943816514362
Main <freshekt@gmail.com>
2024-03-15 21:00:46 +0400

    update settings component

--- src/features/settings/components/settings.component.tsx
+++ src/features/settings/components/settings.component.tsx
@@ -1,27 +1,37 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Text, View} from "react-native";
 import {useTranslation} from "react-i18next";
-import {Picker} from "@react-native-picker/picker";
 import {useSettingsViewModel} from "../viewmodels/settings.viewmodel";
-import styles from "./styles";
+import DropDown from "react-native-paper-dropdown";
+
 export default function SettingsComponent () {
+    const [showDropDown, setShowDropDown] = useState(false);
 
     const {t} = useTranslation();
     const {getSettings, changeLanguage} = useSettingsViewModel();
     const {data} = getSettings();
-
+    const languageList = [
+        {
+            label: t('LANGUAGE_RU'),
+            value: "ru-Ru",
+        },
+        {
+            label: t('LANGUAGE_EN'),
+            value: "en",
+        }
+    ]
     return <View>
-        <Text>{t('LANGUAGE')}</Text>
-        <Picker
-            style={styles.picker}
-            selectedValue={data?.language || 'ru-Ru'}
-            onValueChange={(itemValue, itemIndex) =>
-                changeLanguage(itemValue)
-            }>
-            <Picker.Item label="---"  />
-            <Picker.Item label={t('LANGUAGE_RU')} value="ru-RU" />
-            <Picker.Item label={t('LANGUAGE_EN')} value="en" />
-        </Picker>
+        <DropDown
+            label={t('LANGUAGE')}
+            mode={"outlined"}
+            visible={showDropDown}
+            showDropDown={() => setShowDropDown(true)}
+            onDismiss={() => setShowDropDown(false)}
+            value={'ru-Ru'}
+            setValue={changeLanguage}
+            list={languageList}
+        />
+
 
     </View>
 }
