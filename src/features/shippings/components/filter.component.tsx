3284d142c5636879adc1d455357045e40ba30874
Main <freshekt@gmail.com>
2024-03-15 21:18:24 +0400

    filter changenges

--- src/features/shippings/components/filter.component.tsx
+++ src/features/shippings/components/filter.component.tsx
@@ -1,30 +1,55 @@
 import {Text, TouchableHighlight, View} from "react-native";
 import {Picker} from "@react-native-picker/picker";
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {TransportType} from "../../../shared/models/TransportModel";
 import styles from "./styles";
+import {ShippingViewModelContext} from "../viewmodels/shipping.viewmodel";
+import DropDown from "react-native-paper-dropdown";
 export default function FilterComponent() {
-    const  [transportType, setTransportType] = useState(TransportType.NONE);
+    const [showDropDown, setShowDropDown] = useState(false);
+
     const {t} = useTranslation();
+    const { changeTransportType,transportType,setTransportType} = useContext(ShippingViewModelContext);
+    const  types = [
+        {
+            label: "---",
+            value: TransportType.NONE,
+        },
+        {
+            label: t('FREIGHT'),
+            value: TransportType.FREIGHT,
+        },
+        {
+            label: t('PASSENGER'),
+            value: TransportType.PASSENGER,
+        },
+        {
+            label: t('SPECIAL'),
+            value: TransportType.SPECIAL,
+        },
+    ]
     return (
-        <View style={{flexDirection:'row'}}>
-            <Picker
-                style={styles.picker}
-                selectedValue={transportType}
-                onValueChange={(itemValue, itemIndex) =>
-                    setTransportType(itemValue)
-                }>
-                <Picker.Item label="---" value={TransportType.NONE}  />
-                <Picker.Item label={t('FREIGHT')} value={TransportType.FREIGHT} />
-                <Picker.Item label={t('PASSENGER')} value={TransportType.PASSENGER} />
-                <Picker.Item label={t('SPECIAL')} value={TransportType.SPECIAL} />
-            </Picker>
+        <View style={{flexDirection:'row', justifyContent: 'space-between'}}>
+            <View  style={styles.picker}><DropDown
+
+                label={t('TRANSPORT_TYPE')}
+                mode={"outlined"}
+                visible={showDropDown}
+                showDropDown={() => setShowDropDown(true)}
+                onDismiss={() => setShowDropDown(false)}
+                value={transportType}
+                setValue={setTransportType}
+                list={types}
+            /></View>
             <TouchableHighlight
                 style={styles.apply}
-                onPress={()=>{}}
+                onPress={()=>{
+                    console.log({transportType});
+                    changeTransportType(transportType);
+                }}
                 underlayColor='#fff'>
-                <Text style={{textAlign:'center'}}>{t('APPLY')}</Text>
+                <Text style={{textAlign:'center', color: '#efefef'}}>{t('APPLY')}</Text>
             </TouchableHighlight>
         </View>
     );
