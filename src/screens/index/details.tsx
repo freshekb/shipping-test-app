35079b7d87c9b092a7641005ee1cd6c265ef1f89
Main <freshekt@gmail.com>
2024-03-15 21:19:47 +0400

    details screen

---
+++ src/screens/index/details.tsx
@@ -0,0 +1,7 @@
+
+import React, {useRef} from "react";
+import DetailsComponent from "../../features/shippings/components/details.component";
+
+export default function ShippingItem() {
+    return <DetailsComponent/>
+}
