4293c2ba33b98dfa740782d98df42e5f18583174
Main <freshekt@gmail.com>
2024-03-15 21:20:01 +0400

    shipping

--- src/features/shippings/viewmodels/shipping.viewmodel.ts
+++ src/features/shippings/viewmodels/shipping.viewmodel.ts
@@ -1,11 +1,17 @@
-import {useQuery, useQueryClient, UseQueryResult} from "@tanstack/react-query";
+import {queryOptions, useQuery, useQueryClient, UseQueryOptions, UseQueryResult} from "@tanstack/react-query";
 import {ShippingModel} from "../../../shared/models/ShippingModel";
 import {TransportType} from "../../../shared/models/TransportModel";
-import {getShippingList} from "../../../shared/services/shipping.service";
+import {getShippingItem, getShippingList} from "../../../shared/services/shipping.service";
+import {createContext, Dispatch, SetStateAction, useState} from "react";
 
 
 export  interface ShippingViewModel {
-    getShipping: (type: TransportType) => UseQueryResult<ShippingModel[], Error>;
+    transportType: TransportType;
+    setTransportType: Dispatch<SetStateAction<TransportType>>;
+    getOptions: (type: TransportType) => UseQueryOptions<ShippingModel[], Error>;
+    getShipping: (type?: TransportType | undefined) => UseQueryResult<ShippingModel[], Error>;
+    getShippingById: (id: number) => UseQueryResult<ShippingModel, Error>;
+    changeTransportType: (type: TransportType) => void;
 }
 
 /**
@@ -13,20 +19,61 @@ export  interface ShippingViewModel {
  * useShippingViewModel - returns settings view model functions
  * -------------------------------------------------------------
  */
-export const useShippingViewModel: ShippingViewModel = () => {
+export const useShippingViewModel = (): ShippingViewModel => {
+    const [transportType, setTransportType] = useState(TransportType.NONE);
     // Access the client
     const queryClient = useQueryClient();
-
     /**
-     * getShipping -
+     * getOptions - get shipping list options by type
      * @param type
      */
-    const  getShipping = (type: TransportType = TransportType.NONE) => useQuery({
-        queryKey: ['getSettings',type],
-        queryFn:() => getShippingList(type)
-    });
+    const getOptions = (type: TransportType = TransportType.NONE) =>
+        queryOptions<ShippingModel[], Error>({
+            initialData: [],
+            queryKey: ['getShipping'],
+            queryFn: () =>  getShippingList(type)
+        });
+
+    /**
+     * getShipping - get shipping list by type
+     * @param type
+     */
+    const getShipping = () =>
+        useQuery<ShippingModel[], Error>(getOptions(transportType));
+
+    /**
+     * getShippingItem - get shipping item by id
+     * @param id : number  - id of shipping
+     */
+    const getShippingById = (id: number) =>
+        useQuery<ShippingModel, Error>({
+            queryKey: ['getShippingById',id],
+            queryFn: () =>  getShippingItem(id)
+        });
+
+    /**
+     * changeTransportType - filter transports
+     * @param type : TransportType type of transport
+     */
+    const changeTransportType = async (type: TransportType) => {
+        try {
+            console.log(['getShipping', type]);
+            setTransportType(type);
+            await queryClient.invalidateQueries({queryKey: ['getShipping'], refetchType: "all"});
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
 
     return {
-        getShipping
+        getOptions,
+        getShipping,
+        changeTransportType,
+        transportType,
+        setTransportType,
+        getShippingById
     }
 }
+
+export const ShippingViewModelContext = createContext(useShippingViewModel());
