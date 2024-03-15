1436e20118b916d903876c86a43cab276cbed569
Main <freshekt@gmail.com>
2024-03-15 21:19:06 +0400

    map view

--- src/features/shippings/components/map.component.tsx
+++ src/features/shippings/components/map.component.tsx
@@ -1,7 +1,50 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, {useCallback, useContext, useRef} from "react";
+import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
 import styles from "./styles";
+import {ShippingViewModelContext} from "../viewmodels/shipping.viewmodel";
+import {TransportType} from "../../../shared/models/TransportModel";
+import {ImageURISource} from "react-native";
+import {useFocusEffect} from "expo-router";
+import {getIcon} from "../../../shared/utils/get-icon";
+
 
 export default function MapComponent () {
-    return <MapView style={styles.map} ></MapView>
+    const mapRef = useRef<MapView | null>(null);
+
+    const {getShipping} = useContext(ShippingViewModelContext);
+
+    const {data} = getShipping();
+
+    useFocusEffect(useCallback(()=> {
+        mapRef.current?.fitToElements({
+            animated: true,
+            edgePadding: {
+                bottom: 150,
+                left: 150,
+                right: 150,
+                top: 150,
+            },
+        });
+    }, [data]));
+
+
+    return (<MapView
+        ref={mapRef}
+        style={styles.map}
+        onMapReady={() => {
+             setTimeout(()=>  mapRef.current?.fitToElements({
+                    animated: true,
+                    edgePadding: {
+                        bottom: 150,
+                        left: 150,
+                        right: 150,
+                        top: 150,
+                    },
+                }), 100);
+        }}
+    >
+        {data?.map((item, index) => (
+            <Marker key={index} identifier={`transport${index}`} coordinate={item.coordinates} style={{width: 20}}
+                    image={getIcon(item.transport.type)}/>))}
+    </MapView>);
 }
